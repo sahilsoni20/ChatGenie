8694dcf0d61c10045a2142e7d21f1ecb70597281
sahilsoni20 <ssahil3120@gmail.com>
2024-09-13 18:36:06 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-4">
+        <h1 className="text-3xl font-bold">Hello, Tailwind!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;

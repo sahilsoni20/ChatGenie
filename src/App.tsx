854c40530ffd5a95f404f95c83068d1136fbbc73
sahilsoni20 <ssahil3120@gmail.com>
2024-09-14 12:13:24 +0530

    toggle functinality

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React from "react";
-import "./index.css";
+import { IoChatbubbleEllipsesSharp } from "react-icons/io5";
+import { useState } from "react";
 import { ChatBotContainer } from "./ChatBotContainer";
 
-const App: React.FC = () => {
+const App = () => {
+  const [isChatBotVisible, setIsChatBotVisible] = useState(false);
+
+  const toggleChatBot = () => {
+    setIsChatBotVisible((prev) => !prev);
+  };
+
   return (
-    <>
-      <div>
-        <div className="wave">1</div>
-        <div className="wave">2</div>
-        <div className="wave">3</div>
-      </div>
-      <div>
-        <ChatBotContainer/>
-      </div>
-    </>
+    <div>
+      <button onClick={toggleChatBot} className="fixed bottom-4 right-4 md:bottom-8 md:right-8 z-50">
+        {!isChatBotVisible && (
+          <div className="flex items-center justify-center bg-indigo-600 shadow-2xl rounded-2xl p-3">
+            <IoChatbubbleEllipsesSharp color="white" size={30} />
+          </div>
+        )}
+      </button>
+
+      {/* ChatBotContainer or any other component can go here */}
+      {isChatBotVisible && <ChatBotContainer onClose={toggleChatBot} />}
+    </div>
   );
 };
 
